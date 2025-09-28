0776fed3db7470e7fe29b2896898f50872578138
Stoic <sme.nik.dov@gmail.com>
2025-09-28 16:26:51 +0500

    feat(task3): complete the task 3

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,52 @@
+import url from 'url';
 import RoutesGroup from '../services/routesGroup.js';
+import { redis, httpClient } from '../dependencies.js';
 
 const routes = new RoutesGroup();
 
 routes.get('/ping', {
     handler: (req, res) => {
         return { ping: 'pong' };
+    },
+});
+
+routes.get('/weather', {
+    handler: async (req, res) => {
+        try {
+            const parsedUrl = url.parse(req.url, true);
+            const city = parsedUrl.query.city;
+
+            if (!city) {
+                return { error: 'City is required' };
+            }
+
+            const cacheKey = 'weather:' + city.toLowerCase();
+            const cachedData = await redis.get(cacheKey);
+
+            if (cachedData !== null) {
+                const weatherData = JSON.parse(cachedData);
+                return weatherData;
+            }
+
+            const geoData = await httpClient.get(`http://geocoding-api.open-meteo.com/v1/search?name=${encodeURIComponent(city)}`);
+
+            console.log(geoData);
+
+            const { latitude: lat, longitude: lon } = geoData.data.results[0];
+            const forecastData = await httpClient.get(`http://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=temperature_2m&forecast_hours=24`);
+
+            console.log(forecastData);
+
+            const weatherData = forecastData.data.hourly;
+
+            await redis.set(cacheKey, JSON.stringify(weatherData), 900);
+
+            return weatherData;
+        }
+        catch (error) {
+            console.error(error)
+            return { error: error.message };
+        }
     },
 });
 
